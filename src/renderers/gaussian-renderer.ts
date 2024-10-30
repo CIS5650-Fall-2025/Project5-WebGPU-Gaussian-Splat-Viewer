3d1ac3bcf11bfa9778535658b11727e813e0e9ee
jms44 <jms44@umd.edu>
2024-10-29 21:53:30 -0400

    working alright, still need to add variable gaussian size

--- src/renderers/gaussian-renderer.ts
+++ src/renderers/gaussian-renderer.ts
@@ -5,7 +5,7 @@ import { get_sorter,c_histogram_block_rows,C } from '../sort/sort';
 import { Renderer } from './renderer';
 
 export interface GaussianRenderer extends Renderer {
-
+  render_settings_buffer: GPUBuffer;
 }
 
 // Utility to create GPU buffers
@@ -34,11 +34,71 @@ export default function get_renderer(
   //            Initialize GPU Buffers
   // ===============================================
 
-  const nulling_data = new Uint32Array([0]);
+
+
+  const ind_draw_data = new Uint32Array([4, pc.num_points, 0, 0]);
+
+  const zero_data = new Uint32Array([0]);
+
+  const zero_buffer = createBuffer(
+    device, 'zero_buffer', 4, GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST, 
+    zero_data);
+
+  const ind_draw_buf = createBuffer(
+    device, 
+    'ind_draw_buf',
+    16,
+    GPUBufferUsage.INDIRECT | GPUBufferUsage.COPY_DST,
+  );
+
+  device.queue.writeBuffer(ind_draw_buf, 0, ind_draw_data);
+
+  //render settings
+  const renderSettings = new Float32Array([1.0, pc.sh_deg]); // Example values for gaussian_scaling and sh_deg
+
+  const render_settings_buffer = createBuffer(
+    device,
+    'render_settings',
+    renderSettings.byteLength,
+    GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+    renderSettings
+  );
+
+  //splat buffer
+  const splat_buffer = device.createBuffer({
+    label: 'splat buffer',
+    size: pc.num_points * 7 * 4, // Adjust size based on what data you store per splat
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  });
 
   // ===============================================
   //    Create Compute Pipeline and Bind Groups
   // ===============================================
+
+
+  /*
+  const sort_bind_group_layout = device.createBindGroupLayout({
+    entries: [
+      { binding: 0, visibility: GPUShaderStage.COMPUTE, buffer: { type: 'storage' } },
+      { binding: 1, visibility: GPUShaderStage.COMPUTE, buffer: { type: 'storage' } },
+      { binding: 2, visibility: GPUShaderStage.COMPUTE, buffer: { type: 'storage' } },
+      { binding: 3, visibility: GPUShaderStage.COMPUTE, buffer: { type: 'storage' } },
+    ],
+  });
+  */
+
+
+  /*
+  const preprocess_bind_group_layout = device.createBindGroupLayout({
+    entries: [
+      { binding: 0, visibility: GPUShaderStage.COMPUTE, buffer: { type: 'uniform' } },
+      { binding: 1, visibility: GPUShaderStage.COMPUTE, buffer: { type: 'uniform' } },
+      { binding: 2, visibility: GPUShaderStage.COMPUTE, buffer: { type: 'read-only-storage' } },
+      { binding: 3, visibility: GPUShaderStage.COMPUTE, buffer: { type: 'storage' } },
+    ],
+  });
+  */
+
   const preprocess_pipeline = device.createComputePipeline({
     label: 'preprocess',
     layout: 'auto',
@@ -52,9 +112,21 @@ export default function get_renderer(
     },
   });
 
+  const preprocess_bind_group = device.createBindGroup({
+    label: 'preprocess bind group',
+    layout: preprocess_pipeline.getBindGroupLayout(0),
+    entries: [
+      { binding: 0, resource: { buffer: pc.gaussian_3d_buffer } },
+      { binding: 1, resource: { buffer: splat_buffer } }, // splat buffer for writing
+      { binding: 2, resource: { buffer: camera_buffer } },
+      { binding: 3, resource: { buffer: render_settings_buffer} },
+      { binding: 4, resource: { buffer: pc.sh_buffer } },
+    ],
+  });
+
   const sort_bind_group = device.createBindGroup({
     label: 'sort',
-    layout: preprocess_pipeline.getBindGroupLayout(2),
+    layout: preprocess_pipeline.getBindGroupLayout(1),
     entries: [
       { binding: 0, resource: { buffer: sorter.sort_info_buffer } },
       { binding: 1, resource: { buffer: sorter.ping_pong[0].sort_depths_buffer } },
@@ -63,24 +135,153 @@ export default function get_renderer(
     ],
   });
 
+  /*
+  const preprocess_pipeline_layout = device.createPipelineLayout({
+    bindGroupLayouts: [
+      preprocess_bind_group_layout, // This is for group(0)
+      sort_bind_group_layout        // This is for group(1)
+    ],
+  });
+  */
+
+
+  //enable error logging
+  device.onuncapturederror = (error) => {
+    console.error("WebGPU Error:", error);
+  };
+
 
   // ===============================================
   //    Create Render Pipeline and Bind Groups
   // ===============================================
+  /*
+  const render_bind_group_layout = device.createBindGroupLayout({
+    entries: [
+      { binding: 0, visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT, buffer: { type: 'uniform' } },
+      { binding: 1, visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT, buffer: { type: 'read-only-storage' } },
+    ],
+  });
+  */
+
+  const render_shader = device.createShaderModule({code: renderWGSL});
+
+  const render_pipeline = device.createRenderPipeline({
+    label: 'render',
+    layout: 'auto',
+    vertex: {
+      module: render_shader,
+      entryPoint: 'vs_main',
+      buffers: [],
+    },
+    fragment: {
+      module: render_shader,
+      entryPoint: 'fs_main',
+      targets: [{ 
+        format: presentation_format,
+        blend: {
+          color: {
+            srcFactor: 'src-alpha',
+            dstFactor: 'one-minus-src-alpha',
+            operation: 'add',
+          },
+          alpha: {
+            srcFactor: 'one',
+            dstFactor: 'one-minus-src-alpha',
+            operation: 'add',
+          }
+        }
+
+       }],
+    },
+    primitive: {
+      topology: 'triangle-strip',
+    },
+  });
+
+  const render_bind_group = device.createBindGroup({
+    label: 'render bind group',
+    layout: render_pipeline.getBindGroupLayout(0),
+    entries: [
+      { binding: 0, resource: { buffer: camera_buffer } },
+      { binding: 1, resource: { buffer: splat_buffer } }, // splat buffer for reading
+      { binding: 2, resource: { buffer: sorter.ping_pong[0].sort_indices_buffer } },
+    ],
+  });
+
+  /*
+  const render_pipeline_layout = device.createPipelineLayout({
+    bindGroupLayouts: [
+      render_bind_group_layout,
+    ],
+  });
+  */
   
+
+
+  /*
+  const camera_bind_group = device.createBindGroup({
+    label: 'point cloud camera',
+    layout: render_pipeline.getBindGroupLayout(0),
+    entries: [{binding: 0, resource: { buffer: camera_buffer }}],
+  });
+
+  const gaussian_bind_group = device.createBindGroup({
+    label: 'gaussians',
+    layout: render_pipeline.getBindGroupLayout(1),
+    entries: [
+      {binding: 0, resource: { buffer: pc.gaussian_3d_buffer }},
+    ],
+  });
+  */
+
 
   // ===============================================
   //    Command Encoder Functions
   // ===============================================
   
+  const preprocess = (encoder: GPUCommandEncoder) => {
+    const pass = encoder.beginComputePass();
+    pass.setPipeline(preprocess_pipeline);
+    pass.setBindGroup(0, preprocess_bind_group);
+    pass.setBindGroup(1, sort_bind_group);
+    pass.dispatchWorkgroups(Math.ceil(pc.num_points / C.histogram_wg_size));
+    pass.end();
+  };
+  
+  
+  const render = (encoder: GPUCommandEncoder, texture_view: GPUTextureView) => {
+    const pass = encoder.beginRenderPass({
+      label: 'point cloud render',
+      colorAttachments: [
+        {
+          view: texture_view,
+          loadOp: 'clear',
+          storeOp: 'store',
+          clearValue: { r: 0, g: 0, b: 0, a: 1 },
+        }
+      ],
+    });
+    pass.setPipeline(render_pipeline);
+    pass.setBindGroup(0, render_bind_group);
+    pass.drawIndirect(ind_draw_buf, 0);
+    pass.end();
+  };
+  
 
   // ===============================================
   //    Return Render Object
   // ===============================================
+  //must also return render settings buffer, err elsewise
   return {
     frame: (encoder: GPUCommandEncoder, texture_view: GPUTextureView) => {
+      encoder.copyBufferToBuffer(zero_buffer, 0, sorter.sort_dispatch_indirect_buffer, 0, 4);
+      encoder.copyBufferToBuffer(zero_buffer, 0, sorter.sort_info_buffer, 0, 4);
+      preprocess(encoder);
       sorter.sort(encoder);
+      encoder.copyBufferToBuffer(sorter.sort_info_buffer, 0, ind_draw_buf, 4, 4);
+      render(encoder, texture_view);
     },
     camera_buffer,
+    render_settings_buffer,
   };
 }
--- src/shaders/gaussian.wgsl
+++ src/shaders/gaussian.wgsl
@@ -1,22 +1,103 @@
 struct VertexOutput {
     @builtin(position) position: vec4<f32>,
-    //TODO: information passed from vertex shader to fragment shader
+    @location(0) opacity: f32,
+    @location(1) color: vec4<f32>,
+    @location(2) midpoint: vec2<f32>,
+    @location(3) inv_covar_2d: vec3<f32>,
 };
 
 struct Splat {
-    //TODO: information defined in preprocess compute shader
+    //TODO: store information for 2D splat rendering
+    pos_opacity: array<u32,2>,
+    color_rg_ba: array<u32,2>,
+    inv_covar_2d: array<u32,2>,
+    radius: u32,
 };
 
+struct CameraUniforms {
+    view: mat4x4<f32>,
+    view_inv: mat4x4<f32>,
+    proj: mat4x4<f32>,
+    proj_inv: mat4x4<f32>,
+    viewport: vec2<f32>,
+    focal: vec2<f32>
+};
+
+@group(0) @binding(0)
+var<uniform> camera: CameraUniforms;
+
+@group(0) @binding(1)
+var<storage,read> splats: array<Splat>;
+
+@group(0) @binding(2)
+var<storage, read> sort_indices: array<u32>;
+
 @vertex
-fn vs_main(
+fn vs_main(@builtin(vertex_index) vertexIndex: u32, @builtin(instance_index) instanceIndex: u32
 ) -> VertexOutput {
-    //TODO: reconstruct 2D quad based on information from splat, pass 
+
+    let dummy1 = camera.proj;
     var out: VertexOutput;
-    out.position = vec4<f32>(1. ,1. , 0., 1.);
+
+    let ind = sort_indices[instanceIndex];
+    //TODO: reconstruct 2D quad based on information from splat, pass 
+    // Read the splat data for this instance
+    let splat = splats[ind];
+
+    // Unpack the splat data
+    let pos_xy = unpack2x16float(splat.pos_opacity[0]);
+    let pos_zo = unpack2x16float(splat.pos_opacity[1]);
+    let color = vec4<f32>(
+        unpack2x16float(splat.color_rg_ba[0]),
+        unpack2x16float(splat.color_rg_ba[1])
+    );
+    let inv_covar_2d = vec3<f32>(
+        unpack2x16float(splat.inv_covar_2d[0]),
+        unpack2x16float(splat.inv_covar_2d[1]).y
+    );
+    let position_ndc = vec3<f32>(pos_xy.x, pos_xy.y, pos_zo.x);
+    let opacity = pos_zo.y;
+    //let radius = splat.radius * (1 / 1000.0f);
+    //let radius = 0.01;
+    let rad_2d = unpack2x16float(splat.radius) / 1.0;
+
+    // Define quad offsets for the four vertices (adjust size as needed)
+    var quadOffsets = array<vec2<f32>, 4>(
+        vec2<f32>(-rad_2d.x, -rad_2d.y),
+        vec2<f32>(rad_2d.x, -rad_2d.y),
+        vec2<f32>(-rad_2d.x, rad_2d.y),
+        vec2<f32>(rad_2d.x, rad_2d.y),
+    );
+
+    // Calculate final position by offsetting the splat position
+    let quadOffset = quadOffsets[vertexIndex];
+    //let position_pixels = vec2<f32>((position_ndc.x * 0.5 + 0.5) * camera.viewport.x, (position_ndc.y * 0.5 + 0.5) * camera.viewport.y);
+    out.position = vec4<f32>(
+        position_ndc.xy + quadOffset,
+        0,
+        1.0
+    );
+
+    // Pass opacity to the fragment shader
+    out.opacity = opacity;
+    out.color = color;
+    out.inv_covar_2d = vec3f(inv_covar_2d.x, inv_covar_2d.y, inv_covar_2d.z);
+    out.midpoint = position_ndc.xy;
+    out.midpoint.y = -out.midpoint.y; // flip y axis
     return out;
 }
 
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    return vec4<f32>(1.);
+    let dummy = camera.proj;
+    let delta = ((in.position.xy / camera.viewport) * 2.0 - 1.0) - in.midpoint;
+    let delta_pix = vec2<f32>(0.5 * delta.x * camera.viewport.x, 0.5 * delta.y * camera.viewport.y);
+    let expon = -(0.5 * delta_pix.x * delta_pix.x * in.inv_covar_2d.x + delta_pix.x * delta_pix.y * in.inv_covar_2d.y + 0.5 * delta_pix.y * delta_pix.y * in.inv_covar_2d.z);
+    if expon > 0.0 {
+        return vec4<f32>(0.0, 0.0, 0.0, 0.0);
+    }
+
+    let final_a = min(0.999, in.opacity * exp(expon));
+
+    return vec4<f32>(in.color.x, in.color.y, in.color.z, in.color.w) * final_a;//in.opacity);
 }
